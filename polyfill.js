7e97e322e778a68514d1a2b532ec824aeaeeaaaa
Ben Fletcher <ben@Bens-MacBook-Pro.local>
2014-09-23 10:25:46 +0100

    BF: updated events for IE8

--- polyfill.js
+++ polyfill.js
@@ -64,16 +64,53 @@ if (!document.addEventListener) {
     };
 }
 
-if (!Element.prototype.addEventListener) {
+if (window.Element && !Element.prototype.addEventListener) {
     Element.prototype.addEventListener = function(eventName, handler) {
         this.attachEvent('on' + eventName, handler);
     };
 }
 
-if (!Element.prototype.removeEventListener) {
+if (window.Element && !Element.prototype.removeEventListener) {
     Element.prototype.removeEventListener = function(eventName, handler) {
         this.detachEvent('on' + eventName, handler);
     };
+}
+
+if (!window.Element) {
+    // IE8 and below
+    var createElement = document.createElement;
+    document.createElement = function(tagName) {
+        var el = createElement(tagName);
+        el.addEventListener = function(eventName, handler) {
+            this.attachEvent('on' + eventName, handler);
+        }
+        el.removeEventListener = function(eventName, handler) {
+            this.detachEvent('on' + eventName, handler);
+        }
+        return el;
+    }
+    var getElementById = document.getElementById;
+    document.getElementById = function(id) {
+        var el = getElementById(id);
+        el.addEventListener = function(eventName, handler) {
+            this.attachEvent('on' + eventName, handler);
+        }
+        el.removeEventListener = function(eventName, handler) {
+            this.detachEvent('on' + eventName, handler);
+        }
+        return el;
+    }
+    var getElementByTagName = document.getElementByTagName;
+    document.getElementByTagName = function(tagName) {
+        var el = getElementByTagName(tagName);
+        el.addEventListener = function(eventName, handler) {
+            this.attachEvent('on' + eventName, handler);
+        }
+        el.removeEventListener = function(eventName, handler) {
+            this.detachEvent('on' + eventName, handler);
+        }
+        return el;
+    }
 }
 // Adding trim to string
 if(typeof String.prototype.trim !== 'function') {
