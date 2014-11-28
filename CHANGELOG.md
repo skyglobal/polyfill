068f124dba239ef3279c35467982db5aeb2b360b
Ben Fletcher <bjfletcher@gmail.com>
2014-11-28 12:46:11 +0000

    BF: added String.endsWith

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Change Log
 
+## 0.0.7
+
+  * Added String.endsWith
+
 ## 0.0.6
 
   * Added bind polyfill
--- src/String.js
+++ src/String.js
@@ -5,3 +5,9 @@ if (typeof String.prototype.trim !== 'function') {
     };
 }
 
+if (typeof String.prototype.endsWith !== 'function') {
+    String.prototype.endsWith = function(suffix) {
+        return this.indexOf(suffix, this.length - suffix.length) !== -1;
+    };
+}
+
