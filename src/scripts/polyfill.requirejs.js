f9b598618e7c63f3a56c17cffb91a772ab211b89
Peter Mouland <peter.mouland@gmail.com>
2015-02-10 12:59:30 +0000

    fix requirejs

--- src/scripts/polyfill.requirejs.js
+++ src/scripts/polyfill.requirejs.js
@@ -1,7 +1,7 @@
 var polyfill = require('./polyfill');
 
 if (typeof window.define === "function" && window.define.amd) {
-    define('bower_components/bskyb-polyfill/dist/js/polyfill.requirejs', [], function() {
+    define('bower_components/bskyb-polyfill/dist/scripts/polyfill.requirejs', [], function() {
         'use strict';
         return polyfill;
     });
