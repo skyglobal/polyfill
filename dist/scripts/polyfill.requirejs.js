2bc631b0e2b631edc6509eb6b3173ae8d9fcd162
Peter Mouland <peter.mouland@gmail.com>
2015-02-10 13:00:03 +0000

    v0.2.2

--- dist/scripts/polyfill.requirejs.js
+++ dist/scripts/polyfill.requirejs.js
@@ -16,7 +16,7 @@ skyComponents.polyfill = module.exports;
 var polyfill = require('./polyfill');
 
 if (typeof window.define === "function" && window.define.amd) {
-    define('bower_components/bskyb-polyfill/dist/js/polyfill.requirejs', [], function() {
+    define('bower_components/bskyb-polyfill/dist/scripts/polyfill.requirejs', [], function() {
         'use strict';
         return polyfill;
     });
