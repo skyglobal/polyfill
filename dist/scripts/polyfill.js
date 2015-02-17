19a17dfbef928b15e7090144fbc92c4f150f6151
Peter Mouland <peter.mouland@gmail.com>
2015-02-17 16:12:59 +0000

    v0.2.3

--- dist/scripts/polyfill.js
+++ dist/scripts/polyfill.js
@@ -1,5 +1,6 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 require('./polyfills/Array')();
+require('./polyfills/Element')();
 require('./polyfills/events')();
 require('./polyfills/Function')();
 require('./polyfills/hasOwnProperty')();
@@ -12,7 +13,7 @@ module.exports = {}
 if (typeof skyComponents === "undefined") window.skyComponents = {};
 skyComponents.polyfill = module.exports;
 
-},{"./polyfills/Array":2,"./polyfills/Function":3,"./polyfills/Object":4,"./polyfills/String":5,"./polyfills/events":6,"./polyfills/hasOwnProperty":7,"./polyfills/whichIE":8}],2:[function(require,module,exports){
+},{"./polyfills/Array":2,"./polyfills/Element":3,"./polyfills/Function":4,"./polyfills/Object":5,"./polyfills/String":6,"./polyfills/events":7,"./polyfills/hasOwnProperty":8,"./polyfills/whichIE":9}],2:[function(require,module,exports){
 
 module.exports = function(){
 
@@ -52,6 +53,250 @@ module.exports = function(){
     }
 };
 },{}],3:[function(require,module,exports){
+/*
+ * classList.js: Cross-browser full element.classList implementation.
+ * 2014-07-23
+ *
+ * By Eli Grey, http://eligrey.com
+ * Public Domain.
+ * NO WARRANTY EXPRESSED OR IMPLIED. USE AT YOUR OWN RISK.
+ */
+
+/*global self, document, DOMException */
+
+/*! @source http://purl.eligrey.com/github/classList.js/blob/master/classList.js*/
+
+module.exports = function() {
+
+
+if ("document" in self) {
+
+// Full polyfill for browsers with no classList support
+if (!("classList" in document.createElement("_"))) {
+
+(function (view) {
+
+"use strict";
+
+if (!('Element' in view)) return;
+
+var
+      classListProp = "classList"
+    , protoProp = "prototype"
+    , elemCtrProto = view.Element[protoProp]
+    , objCtr = Object
+    , strTrim = String[protoProp].trim || function () {
+        return this.replace(/^\s+|\s+$/g, "");
+    }
+    , arrIndexOf = Array[protoProp].indexOf || function (item) {
+        var
+              i = 0
+            , len = this.length
+        ;
+        for (; i < len; i++) {
+            if (i in this && this[i] === item) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    // Vendors: please allow content code to instantiate DOMExceptions
+    , DOMEx = function (type, message) {
+        this.name = type;
+        this.code = DOMException[type];
+        this.message = message;
+    }
+    , checkTokenAndGetIndex = function (classList, token) {
+        if (token === "") {
+            throw new DOMEx(
+                  "SYNTAX_ERR"
+                , "An invalid or illegal string was specified"
+            );
+        }
+        if (/\s/.test(token)) {
+            throw new DOMEx(
+                  "INVALID_CHARACTER_ERR"
+                , "String contains an invalid character"
+            );
+        }
+        return arrIndexOf.call(classList, token);
+    }
+    , ClassList = function (elem) {
+        var
+              trimmedClasses = strTrim.call(elem.getAttribute("class") || "")
+            , classes = trimmedClasses ? trimmedClasses.split(/\s+/) : []
+            , i = 0
+            , len = classes.length
+        ;
+        for (; i < len; i++) {
+            this.push(classes[i]);
+        }
+        this._updateClassName = function () {
+            elem.setAttribute("class", this.toString());
+        };
+    }
+    , classListProto = ClassList[protoProp] = []
+    , classListGetter = function () {
+        return new ClassList(this);
+    }
+;
+// Most DOMException implementations don't allow calling DOMException's toString()
+// on non-DOMExceptions. Error's toString() is sufficient here.
+DOMEx[protoProp] = Error[protoProp];
+classListProto.item = function (i) {
+    return this[i] || null;
+};
+classListProto.contains = function (token) {
+    token += "";
+    return checkTokenAndGetIndex(this, token) !== -1;
+};
+classListProto.add = function () {
+    var
+          tokens = arguments
+        , i = 0
+        , l = tokens.length
+        , token
+        , updated = false
+    ;
+    do {
+        token = tokens[i] + "";
+        if (checkTokenAndGetIndex(this, token) === -1) {
+            this.push(token);
+            updated = true;
+        }
+    }
+    while (++i < l);
+
+    if (updated) {
+        this._updateClassName();
+    }
+};
+classListProto.remove = function () {
+    var
+          tokens = arguments
+        , i = 0
+        , l = tokens.length
+        , token
+        , updated = false
+        , index
+    ;
+    do {
+        token = tokens[i] + "";
+        index = checkTokenAndGetIndex(this, token);
+        while (index !== -1) {
+            this.splice(index, 1);
+            updated = true;
+            index = checkTokenAndGetIndex(this, token);
+        }
+    }
+    while (++i < l);
+
+    if (updated) {
+        this._updateClassName();
+    }
+};
+classListProto.toggle = function (token, force) {
+    token += "";
+
+    var
+          result = this.contains(token)
+        , method = result ?
+            force !== true && "remove"
+        :
+            force !== false && "add"
+    ;
+
+    if (method) {
+        this[method](token);
+    }
+
+    if (force === true || force === false) {
+        return force;
+    } else {
+        return !result;
+    }
+};
+classListProto.toString = function () {
+    return this.join(" ");
+};
+
+if (objCtr.defineProperty) {
+    var classListPropDesc = {
+          get: classListGetter
+        , enumerable: true
+        , configurable: true
+    };
+    try {
+        objCtr.defineProperty(elemCtrProto, classListProp, classListPropDesc);
+    } catch (ex) { // IE 8 doesn't support enumerable:true
+        if (ex.number === -0x7FF5EC54) {
+            classListPropDesc.enumerable = false;
+            objCtr.defineProperty(elemCtrProto, classListProp, classListPropDesc);
+        }
+    }
+} else if (objCtr[protoProp].__defineGetter__) {
+    elemCtrProto.__defineGetter__(classListProp, classListGetter);
+}
+
+}(self));
+
+} else {
+// There is full or partial native classList support, so just check if we need
+// to normalize the add/remove and toggle APIs.
+
+(function () {
+    "use strict";
+
+    var testElement = document.createElement("_");
+
+    testElement.classList.add("c1", "c2");
+
+    // Polyfill for IE 10/11 and Firefox <26, where classList.add and
+    // classList.remove exist but support only one argument at a time.
+    if (!testElement.classList.contains("c2")) {
+        var createMethod = function(method) {
+            var original = DOMTokenList.prototype[method];
+
+            DOMTokenList.prototype[method] = function(token) {
+                var i, len = arguments.length;
+
+                for (i = 0; i < len; i++) {
+                    token = arguments[i];
+                    original.call(this, token);
+                }
+            };
+        };
+        createMethod('add');
+        createMethod('remove');
+    }
+
+    testElement.classList.toggle("c3", false);
+
+    // Polyfill for IE 10 and Firefox <24, where classList.toggle does not
+    // support the second argument.
+    if (testElement.classList.contains("c3")) {
+        var _toggle = DOMTokenList.prototype.toggle;
+
+        DOMTokenList.prototype.toggle = function(token, force) {
+            if (1 in arguments && !this.contains(token) === !force) {
+                return force;
+            } else {
+                return _toggle.call(this, token);
+            }
+        };
+
+    }
+
+    testElement = null;
+}());
+
+}
+
+}
+
+}
+
+},{}],4:[function(require,module,exports){
 
 module.exports = function(){
 
@@ -70,7 +315,7 @@ module.exports = function(){
         };
     }
 };
-},{}],4:[function(require,module,exports){
+},{}],5:[function(require,module,exports){
 // From https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/keys
 module.exports = function() {
     if (!Object.keys) {
@@ -115,7 +360,7 @@ module.exports = function() {
     }
 }
 
-},{}],5:[function(require,module,exports){
+},{}],6:[function(require,module,exports){
 
 module.exports = function() {
 
@@ -132,7 +377,7 @@ module.exports = function() {
     }
 
 }
-},{}],6:[function(require,module,exports){
+},{}],7:[function(require,module,exports){
 module.exports = function(){
 
     // from Jonathan Neal's Gist https://gist.github.com/jonathantneal/3748027
@@ -171,12 +416,12 @@ module.exports = function(){
 
 
 };
-},{}],7:[function(require,module,exports){
+},{}],8:[function(require,module,exports){
 
 module.exports = function() {
     window.hasOwnProperty = window.hasOwnProperty || Object.prototype.hasOwnProperty;
 }
-},{}],8:[function(require,module,exports){
+},{}],9:[function(require,module,exports){
 
 module.exports = function() {
 
