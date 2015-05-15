6669b1cd59c619fbac0e9dac3f4db14769e1bf2c
Hodgson, Jonathan <Jonathan.Hodgson@bskyb.com>
2015-05-15 15:13:31 +0100

    fix es5-shim path

--- src/scripts/polyfill.js
+++ src/scripts/polyfill.js
@@ -8,8 +8,8 @@ require('./polyfills/String')();
 require('./polyfills/whichIE')();
 require('./polyfills/pageOffset.js')();
 require('./polyfills/rAF.js')();
-require('es5-shim')();
-require('es5-sham')();
+require('./es5-shim/es5-shim.js')();
+require('./es5-shim/es5-sham.js')();
 
 module.exports = {};
 
