d7319fdc1f62aacd6797a023d398784d21c632d6
Ross Warren <rosswarren4@gmail.com>
2015-08-28 14:16:45 +0100

    delete commented out polyfills

--- src/scripts/polyfill.js
+++ src/scripts/polyfill.js
@@ -1,9 +1,6 @@
-require('./polyfills/Array')();
 require('./polyfills/Element')();
 require('./polyfills/events')();
-require('./polyfills/Function')();
 require('./polyfills/hasOwnProperty')();
-require('./polyfills/Object')();
 require('./polyfills/String')();
 require('./polyfills/whichIE')();
 require('./polyfills/pageOffset.js')();
--- src/scripts/polyfills/String.js
+++ src/scripts/polyfills/String.js
@@ -1,11 +1,4 @@
 module.exports = function() {
-    
-    /* INCLUDED IN ES5-SHIM
-    if ( typeof String.prototype.trim !== 'function') {
-        String.prototype.trim = function() {
-            return this.replace(/^\s+|\s+$/g, '');
-        };
-    }*/
 
     if ( typeof String.prototype.endsWith !== 'function') {
         String.prototype.endsWith = function(suffix) {
