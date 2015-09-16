4c63e3497d1ea4de77efe9ca575b3d8841abe996
Ross Warren <rosswarren4@gmail.com>
2015-09-16 15:49:59 +0100

    fix requirejs browserify incompatibility

--- src/scripts/polyfill.js
+++ src/scripts/polyfill.js
@@ -6,8 +6,18 @@ require('./polyfills/whichIE')();
 require('./polyfills/pageOffset.js')();
 require('./polyfills/rAF.js')();
 
-require('es5-shim/es5-shim');
-require('es5-shim/es5-sham');
+if (typeof define === 'function' && define.amd && typeof exports === 'object') {
+    var oldAmd = define.amd;
+    delete define.amd;
+
+    require('es5-shim/es5-shim');
+    require('es5-shim/es5-sham');
+
+    define.amd = oldAmd;
+} else {
+    require('es5-shim/es5-shim');
+    require('es5-shim/es5-sham');
+}
 
 module.exports = {};
 
